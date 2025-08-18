51b24b7e3325861444e33872a52dd5ace1a299d6
M.Tregub <mishatre123@gmail.com>
2025-08-18 15:50:40 +0700

    fix useSSL env boolean coercion error

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,5 +1,6 @@
-import * as z from "zod";
-import { getEnvConfig } from "./env.js";
+import * as z from 'zod';
+
+import { getEnvConfig } from './env.js';
 
 export type S3Config = {
     endPoint: string;
@@ -9,20 +10,20 @@ export type S3Config = {
     secretKey: string;
 
     defaultBucketName?: string;
-}
+};
 
 export const schema = z.object({
     endPoint: z.string().min(1),
     port: z.coerce.number().min(1).max(65535),
-    useSSL: z.coerce.boolean(),
+    useSSL: z.stringbool(),
     accessKey: z.string().min(1),
     secretKey: z.string().min(1),
     region: z.string(),
-    defaultBucketName: z.optional(z.string())
-})
+    defaultBucketName: z.optional(z.string()),
+});
 
 export const getS3EnvConfig = <D extends Partial<S3Config>>(
-    prefix: string, 
-    additionalPrefix?: string, 
-    defaultConfig?: D
+    prefix: string,
+    additionalPrefix?: string,
+    defaultConfig?: D,
 ) => getEnvConfig(schema, prefix, additionalPrefix, defaultConfig);
